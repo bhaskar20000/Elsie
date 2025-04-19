a805180a96deec67c74bd4e07774d12928d44524
bhaskar20000 <bhaskarjavvadi2020@gmail.com>
2025-04-19 09:38:37 +0530

    wfe

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,6 +4,36 @@ const Home = () => {
   return (
     <div className="home-outer-container">
       <h1 className="home-heading">Home</h1>
+      <p>
+        loremTo generate Lorem Ipsum text in VS Code, you can use the
+        "lorem-ipsum" extension or Emmet's shortcut. The extension offers
+        commands like "lorem-ipsum.line", "lorem-ipsum.paragraph", and
+        "lorem-ipsum.multipleParagraphs" to generate different amounts of text.
+        Emmet, on the other hand, allows you to type "lorem" and then tab to
+        insert a paragraph of Lorem Ipsum text. To generate Lorem Ipsum text in
+        VS Code, you can use the "lorem-ipsum" extension or Emmet's shortcut.
+        The extension offers commands like "lorem-ipsum.line",
+        "lorem-ipsum.paragraph", and "lorem-ipsum.multipleParagraphs" to
+        generate different amounts of text. Emmet, on the other hand, allows you
+        to type "lorem" and then tab to insert a paragraph of Lorem Ipsum text.
+        To generate Lorem Ipsum text in VS Code, you can use the "lorem-ipsum"
+        extension or Emmet's shortcut. The extension offers commands like
+        "lorem-ipsum.line", "lorem-ipsum.paragraph", and
+        "lorem-ipsum.multipleParagraphs" to generate different amounts of text.
+        Emmet, on the other hand, allows you to type "lorem" and then tab to
+        insert a paragraph of Lorem Ipsum text. To generate Lorem Ipsum text in
+        VS Code, you can use the "lorem-ipsum" extension or Emmet's shortcut.
+        The extension offers commands like "lorem-ipsum.line",
+        "lorem-ipsum.paragraph", and "lorem-ipsum.multipleParagraphs" to
+        generate different amounts of text. Emmet, on the other hand, allows you
+        to type "lorem" and then tab to insert a paragraph of Lorem Ipsum text
+        To generate Lorem Ipsum text in VS Code, you can use the "lorem-ipsum"
+        extension or Emmet's shortcut. The extension offers commands like
+        "lorem-ipsum.line", "lorem-ipsum.paragraph", and
+        "lorem-ipsum.multipleParagraphs" to generate different amounts of text.
+        Emmet, on the other hand, allows you to type "lorem" and then tab to
+        insert a paragraph of Lorem Ipsum text.
+      </p>
     </div>
   );
 };
